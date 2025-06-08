9bfef3fb7afe0f7a58f132b94bc507d122856fdc
Lina-Faikun <fajkunleonila@gmail.com>
2025-06-08 21:22:00 +0200

    check

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as contactsController from '../controllers/contacts.js';
-import validateQuery from '../middlewares/validateQuery.js'; 
+// import validateQuery from '../middlewares/validateQuery.js'; 
 import validateId from '../middlewares/validateId.js';
 import validateBody from '../middlewares/validateBody.js';
 import { contactSchema, updateContactSchema } from '../schemas/contactSchema.js'; 
 
 const router = express.Router();
 
-router.get('/', validateQuery, contactsController.getAllContacts);
+router.get('/', contactsController.getAllContacts);
+
 router.get('/:contactId', validateId, contactsController.getContactById);
 router.post('/', validateBody(contactSchema), contactsController.createContact);
 router.patch('/:contactId', validateId, validateBody(updateContactSchema), contactsController.updateContact);
