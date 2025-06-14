c058a543568de3900b08f75971015e883e2e52b8
Lina-Faikun <fajkunleonila@gmail.com>
2025-06-15 00:52:16 +0200

    h[w-5

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,4 +1,3 @@
-// src/middlewares/authenticate.js
 import jwt from "jsonwebtoken";
 import createError from "http-errors";
 import User from "../models/user.js";
@@ -7,27 +6,47 @@ import Session from "../models/session.js";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "accessSecret123";
 
 const authenticate = async (req, res, next) => {
-  const authHeader = req.headers.authorization || "";
-  const token = authHeader.replace("Bearer ", "");
-
-  if (!token) return next(createError(401, "No access token provided"));
-
   try {
-    const payload = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    const authHeader = req.headers.authorization || "";
+    const [bearer, token] = authHeader.split(" ");
 
-    const session = await Session.findOne({ userId: payload.userId, accessToken: token });
-    if (!session) return next(createError(401, "Session invalid or expired"));
+   
+    if (bearer !== "Bearer" || !token) {
+      throw createError(401, "No access token provided");
+    }
 
+   
+    let payload;
+    try {
+      payload = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    } catch (err) {
+      if (err.name === "TokenExpiredError") {
+        throw createError(401, "Access token expired");
+      }
+      throw createError(401, "Invalid token");
+    }
+
+    
+    const session = await Session.findOne({
+      userId: payload.userId,
+      accessToken: token,
+    });
+
+    if (!session) {
+      throw createError(401, "Session invalid or expired");
+    }
+
+  
     const user = await User.findById(payload.userId);
-    if (!user) return next(createError(401, "User not found"));
+    if (!user) {
+      throw createError(401, "User not found");
+    }
+
 
     req.user = user;
     next();
-  } catch (err) {
-    if (err.name === "TokenExpiredError") {
-      return next(createError(401, "Access token expired"));
-    }
-    return next(createError(401, "Invalid token"));
+  } catch (error) {
+    next(error);
   }
 };
 
