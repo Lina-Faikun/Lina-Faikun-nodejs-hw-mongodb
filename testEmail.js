9fd7a05142c8d763f96ea57738c6ba3a86a6e30e
Lina-Faikun <fajkunleonila@gmail.com>
2025-06-17 17:37:17 +0200

    h-w6-check

--- testEmail.js
+++ testEmail.js
@@ -7,7 +7,7 @@ async function testEmail() {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: +process.env.SMTP_PORT,
-    secure: false, // якщо порт 587, то secure: false
+    secure: false, // 587 порт не шифрує SSL, тому false
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
@@ -18,9 +18,10 @@ async function testEmail() {
     const info = await transporter.sendMail({
       from: process.env.SMTP_FROM,
       to: "lina@example.com", 
-      subject: "Test email from nodemailer",
-      text: "If you got this email, SMTP works correctly!",
+      subject: "Test email from nodemailer via Brevo",
+      text: "Якщо ти отримав цей лист — налаштування SMTP працює!",
     });
+
     console.log("Email sent:", info.messageId);
   } catch (error) {
     console.error("Error sending email:", error);
