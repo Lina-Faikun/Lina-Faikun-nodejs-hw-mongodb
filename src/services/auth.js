a6cfcbe2884757ae80615d1139b81fe4bf8d4b11
Lina-Faikun <fajkunleonila@gmail.com>
2025-06-19 17:25:14 +0200

    fix req

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import createError from "http-errors";
-
+import "dotenv/config";
 import User from "../models/user.js";
 import Session from "../models/session.js";
 
