3b9640be1b5c5f4aef80955b38e247beb7dd9bb8
Lina-Faikun <fajkunleonila@gmail.com>
2025-06-20 00:29:40 +0200

    hw7-push-1

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-// server.js
 import express from "express";
 import cors from "cors";
 import logger from "pino";
 import cookieParser from "cookie-parser";
 
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+const swaggerDocument = YAML.load("./docs/openapi.yaml");
+
 import contactsRouter from "./routers/contacts.js";
-import authRouter from "./routers/auth.js"; 
+import authRouter from "./routers/auth.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 
@@ -14,15 +17,17 @@ const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json({ message: "API is working!" });
 });
 
+app.use("/auth", authRouter);
+app.use("/contacts", contactsRouter);
 
-app.use("/auth", authRouter); 
-app.use("/contacts", contactsRouter); 
+// Swagger route:
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(notFoundHandler);
 app.use(errorHandler);
